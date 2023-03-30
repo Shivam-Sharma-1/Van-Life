de630f1787029c6e99566656e994f46e6ded778e
Shivam77607 <shivamsharma77607@gmail.com>
2023-03-30 11:19:35 +0530

    Added HostVans Route

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Layout from './components/Layout'
 import About from './pages/About'
 import Home from './pages/Home'
 import Dashboard from './pages/Host/Dashboard'
+import HostVanDetail from './pages/Host/HostVanDetail'
+import HostVans from './pages/Host/HostVans'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
 import VanDetail from './pages/Vans/VanDetail'
@@ -25,6 +27,8 @@ function App() {
                     <Route path='host' element={<HostLayout/>}>
                         <Route index element={<Dashboard/>}/>
                         <Route path='income' element={<Income/>}/>
+                        <Route path='vans' element={<HostVans/>}/>
+                        <Route path='vans/:id' element={<HostVanDetail/>}/>
                         <Route path='reviews' element={<Reviews/>}/>
                     </Route>
                 </Route>
---
+++ src/pages/Host/HostVanDetail.jsx
@@ -0,0 +1,7 @@
+function HostVanDetail() {
+    return (
+        <h1>Van Detail</h1>
+    )
+}
+
+export default HostVanDetail
