05e3af2bfdcd63f0fdc0b4484e1990be9ccd535a
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-01 21:44:55 +0530

    added errorElement

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,14 @@
+import { useRouteError } from "react-router-dom"
+
 function Error() {
-    return <h1>An error has ocurred</h1>
+    const error = useRouteError()
+    return (
+        <>
+            <h1>An error has ocurred!</h1>
+            <h3>Error: {error.message}</h3>
+            <h3>{error.status} - {error.statusText}</h3>
+        </>
+    )
 }
 
 export default Error
