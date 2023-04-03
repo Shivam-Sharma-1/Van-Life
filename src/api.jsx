e59e1fdb0c0ba0420de703e8b83297ec4f4444e1
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-03 11:47:51 +0530

    updated api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,7 @@
 async function getVans(id) {
     const url = id ? `/api/vans/${id}` : '/api/vans'
     const res = await fetch(url)
+
     if(!res.ok) {
         throw {
             message: "Failed to fetch vans",
@@ -15,6 +16,7 @@ async function getVans(id) {
 async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : '/api/host/vans'
     const res = await fetch(url)
+
     if(!res.ok) {
         throw {
             message: "Failed to fetch vans",
@@ -26,4 +28,20 @@ async function getHostVans(id) {
     return data.vans
 }
 
-export { getVans, getHostVans }
+async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        {method: "post", body: JSON.stringify(creds)}
+    )
+    
+    if(!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status,
+        }
+    }
+    const data = await res.json()
+    return data
+}
+
+export { getVans, getHostVans, loginUser }
