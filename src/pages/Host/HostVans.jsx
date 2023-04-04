c59101c1a7a0bf0d0c05803afa07910af1e332a6
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-04 21:22:06 +0530

    implemented Suspense, Await, defer in HostVans.jsx

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,42 +1,54 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Await, Link, defer, useLoaderData } from "react-router-dom";
 import { getHostVans } from "../../api";
 import { requiredAuth } from "../../utils";
+import { Suspense } from "react";
 
 async function loader({request}) {
-    await requiredAuth(request)
-    return getHostVans()
+    requiredAuth(request)
+    return defer({ vans: getHostVans()})
 }
 
 function HostVans() {
-    const vans = useLoaderData()
+    const dataPromise = useLoaderData()
 
-    const hostVansEle = vans.map(van => {
-        return (
-            <Link
-                to={van.id}
-                key = {van.id}
-                className="host-van-link-wrapper"
-            >
-                <div className="host-van-single" key={van.id}>
-                    <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-                    <div className="host-van-info">
-                        <h3>{van.name}</h3>
-                        <p>${van.price}/day</p>
+    function renderHostVansElements(vans) {
+        const hostVansEle = vans.map(van => {
+            return (
+                <Link
+                    to={van.id}
+                    key = {van.id}
+                    className="host-van-link-wrapper"
+                >
+                    <div className="host-van-single" key={van.id}>
+                        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                        <div className="host-van-info">
+                            <h3>{van.name}</h3>
+                            <p>${van.price}/day</p>
+                        </div>
                     </div>
+                </Link>
+            )
+        })
+        return (
+            <section className="host-vans">
+                <h1 className="host-vans-title">Your listed vans</h1>
+                <div className="host-vans-list">
+                    <section>
+                        {hostVansEle}
+                    </section>
                 </div>
-            </Link>
+            </section>
         )
-    })
+    } 
+
+    
 
     return (
-        <section className="host-vans">
-            <h1 className="host-vans-title">Your listed vans</h1>
-            <div className="host-vans-list">
-                <section>
-                    {hostVansEle}
-                </section>
-            </div>
-        </section>
+        <Suspense fallback={<h2>Loading...</h2>}>
+            <Await resolve={dataPromise.vans}>
+                {renderHostVansElements}
+            </Await>
+        </Suspense>
     )
 }
 
