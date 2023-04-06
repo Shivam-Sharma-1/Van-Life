85a38bef35dbfed53c5e99a05b584f461b8f0fbb
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-06 21:38:30 +0530

    updated Error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,11 +3,13 @@ import { useRouteError } from "react-router-dom"
 function Error() {
     const error = useRouteError()
     return (
-        <>
+        <div className="error-container">
             <h1>An error has ocurred!</h1>
             <h3>Error: {error.message}</h3>
-            <h3>{error.status} - {error.statusText}</h3>
-        </>
+            {
+                error.status ? <h3>{error.status} - {error.statusText}</h3> : null
+            }
+        </div>
     )
 }
 
