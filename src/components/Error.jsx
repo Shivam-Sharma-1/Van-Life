a3d24fa9b62da29301bf3542d5360da3689bd4f3
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-06 21:44:08 +0530

    added styling to Error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,7 +4,7 @@ function Error() {
     const error = useRouteError()
     return (
         <div className="error-container">
-            <h1>An error has ocurred!</h1>
+            <h1>An error has ocurred❗</h1>
             <h3>Error: {error.message}</h3>
             {
                 error.status ? <h3>{error.status} - {error.statusText}</h3> : null
