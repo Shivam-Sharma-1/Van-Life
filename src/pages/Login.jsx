24f042f0042a86053341995712c2527cc739c460
Shivam77607 <shivamsharma77607@gmail.com>
2023-04-07 11:17:11 +0530

    added button to logOut

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { Form, redirect, useActionData, useLoaderData, useNavigation } from "react-router-dom"
 import { loginUser } from "../api"
+import { useState } from "react"
 
 function loader({ request }) {
     return new URL(request.url).searchParams.get('message')
@@ -25,34 +26,45 @@ function Login() {
     const message = useLoaderData()
     const errorMessage = useActionData()
     const navigation = useNavigation()
+    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('loggedIn'))
+
+    function logOut() {
+        localStorage.removeItem('loggedIn')
+        setIsLoggedIn(false)
+    }
 
     return (
         <div className="login-container">
-            <h1>Sign in to your account</h1>
-            {message && <h3 className="red">{message}</h3>}
-            {errorMessage && <h3 className="red">{errorMessage}</h3>}
+            { isLoggedIn ? 
+                <button className="btn-logout" onClick={logOut}>Log Out</button>
+                :
+            (<>
+                <h1>Sign in to your account</h1> 
+                {message && <h3 className="red">{message}</h3>}
+                {errorMessage && <h3 className="red">{errorMessage}</h3>}
 
-            <Form 
-                method='post' 
-                className="login-form"
-                replace
-            >
-                <input
-                    name="email"
-                    type="email"
-                    placeholder="Email address"
-                />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                />
-                <button 
-                    disabled={navigation.state === 'submitting'}
+                <Form 
+                    method='post' 
+                    className="login-form"
+                    replace
                 >
-                    {navigation.state === 'submitting' ? "Logging in..." : "Log in"}
-                </button>
-            </Form>
+                    <input
+                        name="email"
+                        type="email"
+                        placeholder="Email address"
+                    />
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <button 
+                        disabled={navigation.state === 'submitting'}
+                    >
+                        {navigation.state === 'submitting' ? "Logging in..." : "Log in"}
+                    </button>
+                </Form>
+                </>)}
         </div>
     )
 }
